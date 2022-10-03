6313b18f7ae0a327312b8b6d3a1c204cb691fabe
Tina Huynh <tinamchuynh@gmail.com>
2022-10-03 12:23:59 -0700

    Add operators

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import CalcContext from '../context/CalcContext'
+
+const getStyleName = btn => {
+
+
+    const className = {
+        '=': 'equals',
+        "X": 'opt',
+        "-": 'opt',
+        "+": 'opt',
+        "/": 'opt',
+    }
+    return className[btn]
+}
 
 const Button = ({value}) => {
+
+    const {calc, setCalc } = useContext(CalcContext);
+
+
+    const commaClick =() => {
+        setCalc({
+            ...calc,
+            num: !calc.num.toString().includes('.') ? calc.num + value :calc.num
+        });
+        const resetClick = () =>{
+            setCalc ({sign: '', num: 0, res: 0})
+        }
+    }
+
+    const handleBtnClick = () => {
+        const results = {
+        '.': commaClick
+        "C":resetClick
+        }
+        return results[value]()
+
+    }
   return (
-    <button className="button">{value}</button>
+    <button onClick={handleBtnClick} className={`${getStyleName(value)} button`}>{value}</button>
   )
 }
 
