f466717f76bf671280b3439491d87d235eb8d607
Tina Huynh <tinamchuynh@gmail.com>
2022-10-03 11:53:20 -0700

    map through the button values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
 
 const btnValues = [
   ["v", "+-", "%", "/"],
   [7, 8, 9, "X"],
-[4, 5, 6, "-"],
+  [4, 5, 6, "-"],
   [1, 2, 3, "+"],
   [0, ".", "="],
 ]
@@ -13,8 +15,16 @@ function App() {
   return (
     <div>
       <Wrapper>
-        <Screen/>
-          
+        <Screen />
+        <ButtonBox>
+          {btnValues.flat().map((btn, i) => (
+            <Button
+            value = {btn}
+              key = {i}
+            />
+              
+          ))}
+        </ButtonBox>
       </Wrapper>
 
     </div>
