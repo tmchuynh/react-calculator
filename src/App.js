f2c1cc3fc2cd826953a39e8dbf0bf7ac5ff0741f
Tina Huynh <tinamchuynh@gmail.com>
2022-10-03 11:40:26 -0700

    Include button values

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 
+const btnValues = [
+  ["v", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+[4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+]
+
 function App() {
   return (
     <div>
       <Wrapper>
         <Screen/>
-          <h1>App</h1>
+          
       </Wrapper>
 
     </div>
