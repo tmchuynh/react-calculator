0f4f56c91392223392488198df5fbb43f545e041
Tina Huynh <tinamchuynh@gmail.com>
2022-10-03 12:45:47 -0700

    Reformat

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,14 +1,14 @@
-import CalcContext from "../context/CalcContext"
 import { useContext } from "react"
+import { CalcContext } from "../context/CalcContext"
+import { Textfit } from 'react-textfit';
 import React from "react";
-import {Textfit }   from 'react-textfit';
 
 const Screen = () => {
+  const { calc } = useContext(CalcContext);
 
-    const {calc } = useContext(CalcContext);
-    return (
-        <Textfit className="screen" max= {70} mode="single">{calc.num ? calc.num : calc.res}</Textfit>
-    )
+  return (
+    <Textfit className="screen" max={70} mode="single">{calc.num ? calc.num : calc.res}</Textfit>
+  )
 }
 
 export default Screen
