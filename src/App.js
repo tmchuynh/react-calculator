9660700a747c2507aa7ca9bac7a54f6c0e8749d4
Tina Huynh <tinamchuynh@gmail.com>
2022-10-03 12:45:00 -0700

    update imports

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
-import ButtonBox from "./components/ButtonBox";
-import Button from "./components/Button";
+import ButtonBox from './components/ButtonBox';
+import React from "react";
+import Button from './components/Button';
 import CalcProvider from "./context/CalcContext";
 
 const btnValues = [
-  ["v", "+-", "%", "/"],
-  [7, 8, 9, "X"],
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
   [4, 5, 6, "-"],
   [1, 2, 3, "+"],
   [0, ".", "="],
-]
+];
 
 function App() {
   return (
